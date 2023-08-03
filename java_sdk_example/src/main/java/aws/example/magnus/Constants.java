87409c09a98cd842e9608c98a44334bd51f6cbb0
Amit Khanal <akhnal@amazon.com>
2023-08-03 13:19:45 -0700

    clean up

--- java_sdk_example/src/main/java/aws/example/magnus/Constants.java
+++
@@ -1,7 +0,0 @@
-package aws.example.magnus;
-
-public class Constants {
-    public static final String REGION = "us-east-2";
-    public static final String CONTROL_ENDPOINT = "https://controlplane.preprod.magnus.us-east-2.amazonaws.com";
-    public static final String DATA_ENDPOINT = "https://dataplane.preprod.magnus.us-east-2.amazonaws.com";
-}
